d9f91c6a29b4e4c8a048d1a605271ab3a3588464
Francesco Pasa <francescopasa@gmail.com>
2014-11-11 21:49:18 +0100

    ciao

--- numinger.py
+++ numinger.py
@@ -29,21 +29,68 @@ def numerov_integration(domain, a, f0, f1):
 
     return f
 
-def schrod(domain, V, boundary_start, boundary_end, m=1.0, dE=0.1, E_max=10.0):
+def schrod(domain, V, boundary_start, boundary_end, m=1.0, dE=0.1, E_max=10.0,
+        eigen_num=None, precision=0.0001):
+
     # Find minimum of potential
     E_min = numpy.min(V)
 
     # Find eigenvalues of energy using the shooting method
+    # The following lines allow the function to accept both a range of energies
+    # or a number of eigenvalues to be found
+    if eigen_num is None:
+        energies = numpy.linspace(E_min, E_max, int((E_max - E_min)/dE))
+    else:
+        # Generator of energies. In the following for loop
+        # we will exit when we have found eigen_num number of eigenvalues
+        def energies():
+            n = 0
+            while True:
+                yield E_min + dE * n
+                n += 1
+
+        energies = energies()
+
+    eigenvalues = []
+    eigenfunctions = []
     err = None
-    for E in numpy.linspace(E_min, E_max, int((E_max - E_min)/dE)):
+    last_E = None
+    for E in energies:
         a = 2.0*m*(V - E) / h**2
 
         psi = numerov_integration(domain, a, boundary_start, boundary_start+1.0)
-        new_err = psi[-1]
-        print(E, new_err)
+        new_err = psi[-1] - boundary_end
 
         if err is not None and err*new_err < 0:
             # We found a energy eigenvalue!
-            print("Eigenvalue:", E)
+            # Now, using the shooting method (bisection), we get the wave function
+            # according to precision
+            error = new_err # error instead of err to avoid confusion with outer loop
+            E1 = last_E
+            E2 = E
+            while abs(error) > precision:
+                E = (E2 + E1) / 2.0
+
+                a = 2.0*m*(V - E) / h**2
+
+                psi = numerov_integration(domain, a, boundary_start, boundary_start+1.0)
+                new_error = psi[-1] - boundary_end
+
+                if error*new_error < 0:
+                    E1 = E2
+                    E2 = E
+                else:
+                    E2 = E
+
+                error = new_error
+
+            eigenvalues.append(E)
+            eigenfunctions.append(psi)
+
+        if eigen_num is not None and len(eigenvalues) == eigen_num:
+            break
 
         err = new_err
+        last_E = E
+
+    return (eigenvalues, eigenfunctions)
--- p.py
+++ p.py
@@ -2,10 +2,20 @@
 
 import numinger as nu
 import numpy as np
+import matplotlib.pyplot as plt
 
 D = nu.Domain(-1, 1, 1000)
-V = np.zeros(len(D))
-V[0] = 1e15
-V[-1] = 1e15
+#V = np.zeros(len(D))
+#V[0] = 1e15
+#V[-1] = 1e15
+V = 0.5*D.as_array()**2
+print(V)
 
-nu.schrod(D, V, 0, 0)
+ev, ef = nu.schrod(D, V, 1, 1, eigen_num=5)
+
+for v, f in zip(ev, ef):
+    plt.plot(D.as_array(), f, label="{:.4}".format(v))
+
+plt.grid(True)
+plt.legend()
+plt.show()
